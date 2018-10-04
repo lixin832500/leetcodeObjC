055880a6cbffac0e8522c775c536a02d85b6febd
Shane Li <lixin832500@gmail.com>
2018-10-04 00:39:27 -0700

    add Q25

---
+++ leetcodeObjC/25-ReverseNodesInKGroup.h
@@ -0,0 +1,16 @@
+//
+//  25-ReverseNodesInKGroup.h
+//  leetcodeObjC
+//
+//  Created by lx on 7/12/1397 AP.
+//  Copyright © 1397 lx. All rights reserved.
+//
+
+#ifndef _5_ReverseNodesInKGroup_h
+#define _5_ReverseNodesInKGroup_h
+#import "ListNode.h"
+
+@interface Solution25 : NSObject
+- (ListNode *) reverseKGroup: (ListNode *) head : (NSInteger) k;
+@end
+#endif /* _5_ReverseNodesInKGroup_h */
---
+++ leetcodeObjC/25-ReverseNodesInKGroup.m
@@ -0,0 +1,17 @@
+//
+//  25-ReverseNodesInKGroup.m
+//  leetcodeObjC
+//
+//  Created by lx on 7/12/1397 AP.
+//  Copyright © 1397 lx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ListNode.h"
+#import "25-ReverseNodesInKGroup.h"
+
+@implementation Solution25 : NSObject
+- (ListNode *) reverseKGroup: (ListNode *) head : (NSInteger) k
+{
+}
+@end
